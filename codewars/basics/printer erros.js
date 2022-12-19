def989131d00fdc84b6f9b6844fe51c2c7882916
Gustavo <gusalv2001@gmail.com>
2022-12-19 11:18:51 -0400

    added 5 katas

---
+++ codewars/basics/printer erros.js
@@ -0,0 +1,14 @@
+// You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters from ato z.
+
+// Examples:
+// s="aaabbbbhaijjjm"
+// printer_error(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// printer_error(s) => "8/22"
+
+function printerError(s) {
+  return (s.match(/[^a-m]/g)?.length || 0) + "/" + s.length;
+}
