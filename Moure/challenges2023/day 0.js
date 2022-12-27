a17d3e5f29dcdaf99848047fcf2232a0832f3d77
gusrkds <gusalv2001@gmail.com>
2022-12-26 21:21:13 -0400

    add day 0 moure challenge 2023

---
+++ Moure/challenges2023/day 0.js
@@ -0,0 +1,22 @@
+/* Reto #0: EL FAMOSO "FIZZ BUZZ”
+FÁCIL | Publicación: 26/12/22 | Resolución: 02/01/23
+
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("mul 3 y 5");
+  } else if (i % 5 === 0) {
+    console.log("mul 5");
+  } else if (i % 3 === 0) {
+    console.log("mul 3");
+  } else {
+    console.log(i);
+  }
+}
