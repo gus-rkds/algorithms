4a441fb1922031c66a8a345447d3a244375696c5
gusrkds <gusalv2001@gmail.com>
2022-12-29 20:47:53 -0400

    ADD count of positives and sum of negatives

--- codewars/basics/Count of positives and sum of negatives.js
+++ codewars/basics/Count of positives and sum of negatives.js
@@ -6,3 +6,12 @@ If the input is an empty array or is null, return an empty array.
 
 Example
 For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65]. */
+
+function countPositivesSumNegatives(a) {
+  if (!a || !a.length) return [];
+
+  let pos = a.filter((x) => x > 0),
+    neg = a.filter((x) => x <= 0);
+
+  return [pos.length, Math.floor(neg.reduce((s, v) => s + v, 0))];
+}
