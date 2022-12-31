ec46af265dde089869a5bd5735b09ce632896651
gusrkds <gusalv2001@gmail.com>
2022-12-31 10:35:44 -0400

    add: highest scoring world pt2

--- codewars/6 kyu/highest scoring world.js
+++ codewars/6 kyu/highest scoring world.js
@@ -8,3 +8,24 @@ You need to return the highest scoring word as a string.
 If two words score the same, return the word that appears earliest in the original string.
 
 All letters will be lowercase and all inputs will be valid. */
+
+function high(words) {
+  const alpha = ' abcdefghijklmnopqrstuvwxyz';
+  const score = (word) =>
+    word.split('').reduce((a, b) => a + alpha.indexOf(b), 0);
+
+  return words
+    .split(' ')
+    .map((word, pos) => ({ word: word, pos: pos, score: score(word) }))
+    .sort((a, b) => a.score - b.score || b.pos - a.pos)
+    .pop().word;
+}
+
+// or
+
+function highest(s) {
+  let as = s
+    .split(' ')
+    .map((s) => [...s].reduce((a, b) => a + b.charCodeAt(0) - 96, 0));
+  return s.split(' ')[as.indexOf(Math.max(...as))];
+}
